f9914c7cf3731bea0d8dca8524936de6cb21453b
beleriandcrises <beleriandcrises@pokemail.net>
2019-09-13 21:12:42 +0200

    First commit with all the files

---
+++ chloro/PAUP-2019.09.13-Run.1/Script.bat
@@ -0,0 +1,4 @@
+cd \/home\/tombom\/Documents\/uni\/filogenesiVegetale\/elaborato\/files\/chloro\/PAUP-2019.09.13-Run.1\/
+
+ "/home/tombom/Documents/uni/filogenesiVegetale/programmi/paup4a166_ubuntu64" PAUPCommands.txt > StandardOutputFile 2> StandardErrorFile
+rm -f \/home\/tombom\/Documents\/uni\/filogenesiVegetale\/elaborato\/files\/chloro\/PAUP-2019.09.13-Run.1\/running
